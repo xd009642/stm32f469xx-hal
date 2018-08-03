0af8d92e58c5dc1345c884547526bd8e42afbcf9
xd009642 <danielmckenna93@gmail.com>
2018-08-03 21:42:50 +0100

    Added impls for ahb 2 & 3 and apb registers.
    Clock configuration and setting isn't yet done but I'll save that for the future!

--- src/rcc.rs
+++ src/rcc.rs
@@ -36,7 +36,6 @@ impl AHB1 {
         unsafe { &(*RCC::ptr()).ahb1enr }
     }
     
-    
     pub(crate) fn rstr(&mut self) -> &rcc::AHB1RSTR {
         unsafe { &(*RCC::ptr()).ahb1rstr }
     }
@@ -46,14 +45,54 @@ pub struct AHB2 {
     _0: (),
 }
 
+impl AHB2 {
+    pub(crate) fn enr(&mut self) -> &rcc::AHB2ENR {
+        unsafe { &(*RCC::ptr()).ahb2enr }
+    }
+    
+    pub(crate) fn rstr(&mut self) -> &rcc::AHB2RSTR {
+        unsafe { &(*RCC::ptr()).ahb2rstr }
+    }
+}
+
 pub struct AHB3 {
     _0: (),
+}
+
+impl AHB3 {
+    pub(crate) fn enr(&mut self) -> &rcc::AHB3ENR {
+        unsafe { &(*RCC::ptr()).ahb3enr }
+    }
+    
+    pub(crate) fn rstr(&mut self) -> &rcc::AHB3RSTR {
+        unsafe { &(*RCC::ptr()).ahb3rstr }
+    }
 }
 
 pub struct APB1 {
     _0: (),
 }
 
+impl APB1 {
+    pub(crate) fn enr(&mut self) -> &rcc::APB1ENR {
+        unsafe { &(*RCC::ptr()).apb1enr }
+    }
+    
+    pub(crate) fn rstr(&mut self) -> &rcc::APB1RSTR {
+        unsafe { &(*RCC::ptr()).apb1rstr }
+    }
+}
+
 pub struct APB2 {
     _0: (),
+}
+
+impl APB2 {
+    pub(crate) fn enr(&mut self) -> &rcc::APB2ENR {
+        unsafe { &(*RCC::ptr()).apb2enr }
+    }
+    
+    pub(crate) fn rstr(&mut self) -> &rcc::APB2RSTR {
+        unsafe { &(*RCC::ptr()).apb2rstr }
+    }
 }
